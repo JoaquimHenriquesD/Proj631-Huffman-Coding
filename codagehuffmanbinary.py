0abdced48b40917e70858bec7aea60a25dbff9f3
Joaquim DOS SANTOS <jd.henriques24@gmail.com>
2022-05-06 11:42:01 +0200

    Update of name

--- codagehuffmanbinary.py
+++ codagehuffmanbinary.py
@@ -1,32 +1,29 @@
-class ConvertisseurBinaire:
-    
-    
-    def ecrireBinaireFichier(self,fileName,contenu):
+class BinaryConverter:
+
+    def writeBinaryFile(self,fileName,contenu):
         with open(fileName, "wb") as f:
             for octet in contenu:
                 f.write((int(octet, base=2)).to_bytes((len(octet)//8), byteorder='big')) 
         f.close()
     
-    def addBitManquant(self,contenu):
-        while len(contenu)%8 != 0:
-            contenu = contenu+"0"
-            
+    def addLastBit(self,content):
+        while len(content)%8 != 0:
+            content = content+"0"
 
-    def get8pack(self,contenu):
-        listeBin = []
-        for i in range(8,len(contenu),8):
-            listeBin.append(contenu[i-8:i])
-        return listeBin
-            
-        
-    def convertEnBinaire(self,text,binaryTable):
-        contenuBinaire = ""
+    def BinaryConvertion(self,text,binaryTable):
+        textBinary = ""
         for i in text:
-            contenuBinaire = contenuBinaire + binaryTable[i]
-        return contenuBinaire
+            textBinary = textBinary + binaryTable[i]
+        return textBinary     
+
+    def get8pack(self,content):
+        listeBin = []
+        for i in range(8,len(content),8):
+            listeBin.append(content[i-8:i])
+        return listeBin    
     
-    def ecrireValBinaire(self,alphabet,binaryTable,nomdufichier):
-        with open(nomdufichier[:-4]+'_valCharBin.txt','a') as f:
+    def writeBinaryValue(self,alphabet,binaryTable,fileName):
+        with open(fileName[:-4]+'_valCharBin.txt','a') as f:
             for i in alphabet:
                 f.write(i+" : "+str(binaryTable[i])+'\n')
             f.close()
